94bc9c5782bccbd4e739147af3e0dd08994a1c66
Tariq Odeh <tariqodeh2001@gmail.com>
2023-08-17 14:37:01 +0300

    simple exapmle

--- string_calculator.py
+++ string_calculator.py
@@ -1,15 +1,14 @@
 def string_adder(string_input):
     result = 0
-
     for char in string_input:
         if char.isdigit():
             result += int(char)
-
     return result
 
 
-def test_answer():
+def test_simple():
     assert string_adder("") == 0
     assert string_adder("1") == 1
     assert string_adder("1,2") == 3
-    assert string_adder("1,2,5,6,3,8") == 25
+
+
