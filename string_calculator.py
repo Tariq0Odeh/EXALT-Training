c72ef3cd88e50f402abe78a0b0ab762fef116006
Tariq Odeh <tariqodeh2001@gmail.com>
2023-08-17 14:47:03 +0300

    deal with unknown amount of numbers

--- string_calculator.py
+++ string_calculator.py
@@ -1,14 +1,18 @@
+
+
 def string_adder(string_input):
     result = 0
-    for char in string_input:
-        if char.isdigit():
-            result += int(char)
+    nums = string_input.split(",")
+    for num in nums:
+        result += int(num)
     return result
-
 
 def test_simple():
     assert string_adder("") == 0
     assert string_adder("1") == 1
     assert string_adder("1,2") == 3
 
-
+def test_unknown_amount_numbers():
+    assert string_adder("1,2,5,6,3,8") == 25
+    assert string_adder("0,2,6,12,10") == 30
+    assert string_adder("1,1,1,2,2,5,80") == 92
