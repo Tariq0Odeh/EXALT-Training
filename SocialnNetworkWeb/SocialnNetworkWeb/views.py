57b6d12cdb8636d0d7e5c42257db20d8f2516ec2
Tariq Odeh <tariqodeh2001@gmail.com>
2023-09-10 16:42:56 +0300

    make profile

---
+++ SocialnNetworkWeb/SocialnNetworkWeb/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home_view(request):
+    user = request.user
+    hello = 'Hello world'
+
+    context = {
+        'user': user,
+        'hello': hello,
+    }
+    return render(request, 'base.html', context)
+
+
